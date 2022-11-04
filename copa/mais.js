e64341c65f321cb9a5e51a7ba6cc661b1effc40c
Mykon <helvis89@gmail.com>
2022-11-04 17:05:50 -0300

    second commit

--- copa/mais.js
+++ copa/mais.js
@@ -5,37 +5,29 @@ function createGame(player1, hour, player2) {
       <strong>${hour}</strong>
       <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
     </li>
-  `
+  `;
 }
 
+let delay = -0.3;
+
 function createCard(date, day, games) {
+  delay = delay + 0.3;
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
       <h2>${date}<span>${day}</span></h2>
       <ul>
         ${games}
       </ul>
     </div>  
-  `
+  `;
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="Logo da Nwl" />
-  </header>
-
-  <main id="cards">
-    ${createCard("24/11", "quinta", createGame("brazil", "08:00", "serbia"))}
-    ${createCard(
-      "28/11",
-      "segunda",
-      createGame("switzerland", "13:00", "brazil") +
-        createGame("portugal", "16:00", "uruguay")
-    )}
-    ${createCard(
-      "02/12",
-      "sexta",
-      createGame("brazil", "16:00", "cameroon")
-    )}      
-  </main>
-`
+document.querySelector('#cards').innerHTML =
+  createCard('24/11', 'quinta', createGame('brazil', '08:00', 'serbia')) +
+  createCard(
+    '28/11',
+    'segunda',
+    createGame('switzerland', '13:00', 'brazil') +
+      createGame('portugal', '16:00', 'uruguay')
+  ) +
+  createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameroon'));
